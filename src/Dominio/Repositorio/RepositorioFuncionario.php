6d50f7a191481e78a6d3b5d64fdfad865ace0c40
Bruno Gonçalves <bruno_s_goncalves@hotmail.com>
2022-08-07 01:32:34 -0300

    Fix(ORM): Agora vamos começar com o ORM  !!

--- src/Dominio/Repositorio/RepositorioFuncionario.php
+++ src/Dominio/Repositorio/RepositorioFuncionario.php
@@ -8,6 +8,13 @@ interface repositorioFuncionario
 {
     public function todosFuncionarios():array;    
     public function salva(Funcionario $funcionario): bool;
+    public function createFuncionario(Funcionario $funcionario): bool;
     public function updateFuncionario(Funcionario $funcionario):bool;
+    public function readFuncionario(Funcionario $funcionario):array;
     public function deletateFuncionario(Funcionario $funcionario):bool;
 }
+
+
+/*
+Apenas um teste para ver se funciona
+*/
